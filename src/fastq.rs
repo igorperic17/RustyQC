1deccc79efda0e43261f4c47a93c3930c5cbd5b9
Igor Peric <igorperic@live.com>
2023-08-13 10:17:20 +0200

    Reading only qualities

--- src/fastq.rs
+++ src/fastq.rs
@@ -10,8 +10,14 @@ pub fn read_lines(path: &str) -> std::io::Result<Vec<String>> {
 
     let reader = BufReader::new(file);
     let mut lines = Vec::<String>::new();
+    let mut line_idx: u64 = 0;
     for line in reader.lines() {
-        lines.push(line?);
+        line_idx += 1;
+
+        if line_idx == 4 {
+            lines.push(line?);
+            line_idx = 0;
+        }
     }
 
     Ok(lines)
